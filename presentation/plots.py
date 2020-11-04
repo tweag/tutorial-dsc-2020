2484aae2aa2d0105d56f5f405d41b29693f70413
Simeon Carstens <simeon.carstens@tweag.io>
2020-11-04 20:24:04 +0100

    Add presentation pdf and sources

---
+++ presentation/plots.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+font = {'family' : 'DejaVu Sans',
+        # 'weight' : 'normal',
+        'size'   : 22}
+
+matplotlib.rc('font', **font)
+
+save = True
+
+def show_or_save(filename):
+    plt.savefig(filename) if save else plt.show()
+    plt.close()
+
+def despine(ax):
+    for spine in ('top', 'right'):
+        ax.spines[spine].set_visible(False)
+    
+## Beta(2,2)
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(0, 1, 100)
+ax.plot(xspace, xspace * (1 - xspace) / 6.0)
+ax.set_xlim((0, 1))
+ax.set_ylim((0,0.05))
+ax.set_yticks(())
+ax.set_xlabel('$b$')
+ax.set_ylabel('$p(b)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/coin_flip_prior.pdf')
+
+## l(b)=p(k=1|b)
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(0, 1, 100)
+ax.plot(xspace, xspace)
+ax.set_xlim((0, 1))
+ax.set_ylim((0, 1))
+ax.set_xlabel('$b$')
+ax.set_ylabel('$p(k=1|b)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/coin_flip_likelihood.pdf')
+
+## p(b|k=1) a.k.a. Beta(3,2)
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(0, 1, 100)
+ax.plot(xspace, xspace ** 2 * (1 - xspace))
+ax.set_xlim((0, 1))
+ax.set_ylim((0, 0.2))
+ax.set_yticks(())
+ax.set_xlabel('$b$')
+ax.set_ylabel('$p(b|k=1)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/coin_flip_posterior.pdf')
+
+## normal
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(-3, 3, 100)
+ax.plot(xspace, np.exp(-0.5 * xspace ** 2) / np.sqrt(2 * np.pi), color='black')
+ax.set_xlim((-3, 3))
+ax.set_ylim((0, 0.5))
+ax.set_xlabel('$x$')
+ax.set_ylabel('$p(x)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/normalized_normal.pdf')
+
+## normal and unnormalized normal
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(-3, 3, 100)
+ax.plot(xspace, np.exp(-0.5 * xspace ** 2) / np.sqrt(2 * np.pi), color='black')
+ax.plot(xspace, np.exp(-0.5 * xspace ** 2), color='black', ls='--')
+ax.set_xlim((-3, 3))
+ax.set_ylim((0, 1.1))
+ax.set_xlabel('$x$')
+ax.set_ylabel('$p(x)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/normalized_unnormalized_normal.pdf')
+
+## Bernoulli(0.2)
+fig, ax = plt.subplots(figsize=(5, 4))
+ax.scatter((2, 4), (0.8, 0.2), marker='o')
+ax.axvline(2, ymax=0.8, color='black', ls='--')
+ax.axvline(4, ymax=0.2, color='black', ls='--')
+ax.set_xticks((2, 4))
+ax.set_xticklabels(('0 (tail)', '1 (head)'))
+ax.set_xlim((1, 5))
+ax.set_ylim((0, 1))
+ax.set_xlabel('$k$')
+ax.set_ylabel('$p(k)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/bernoulli.pdf')
+
+## lognormal
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(0.001, 10, 100)
+mu = 2
+sigma = 0.5
+ax.plot(xspace, np.exp(-0.5 * np.log(xspace/mu) ** 2 / sigma / sigma) / xspace / np.sqrt(2 * np.pi * sigma ** 2), color='black')
+ax.set_xlim((0, 10))
+#ax.set_ylim((0, 0.5))
+ax.set_yticks(())
+ax.set_xlabel('$\sigma$')
+ax.set_ylabel('$p(\sigma)$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/lognormal.pdf')
+
+## wide normal
+fig, ax = plt.subplots(figsize=(5, 4))
+xspace = np.linspace(-13, 13, 100)
+ax.plot(xspace, np.exp(-0.5 * xspace ** 2 / 25.) / np.sqrt(2 * np.pi * 25**2),
+        color='black')
+ax.set_xlim((-13, 13))
+ax.set_yticks(())
+ax.set_xlabel(r'$\beta_{i}$')
+ax.set_ylabel(r'$p(\beta_{i})$')
+fig.tight_layout()
+despine(ax)
+show_or_save('images/widenormal.pdf')
