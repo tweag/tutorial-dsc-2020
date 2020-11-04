0fa0404180bf9e7b60042181f18e3b018c6097b6
Matthias Meschede <MMesch@users.noreply.github.com>
2020-11-04 20:00:57 +0100

    iteration on theme

--- presentation/shell.nix
+++ presentation/shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  mytexlive = pkgs.texlive.combine {inherit (pkgs.texlive) scheme-medium pgf xetex  fontspec euenc enumitem pgfplots adjustbox collectbox; };
+  mytexlive = pkgs.texlive.combine {inherit (pkgs.texlive) scheme-medium pgf xetex  fontspec euenc enumitem pgfplots adjustbox collectbox tcolorbox environ trimspaces; };
 
 in
   pkgs.mkShell {
